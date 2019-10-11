8235fe928bbb491b48043005a3d526485261b58e
André Luiz Ribeiro da Silva <aribeiro.rj@gmail.com>
2019-10-11 15:31:44 -0300

    start development chart

--- src/components/chart/index.js
+++ src/components/chart/index.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+// import './Chart.css';
+import '../../../node_modules/react-vis/dist/style.css';
+import {
+  XYPlot,
+  LineMarkSeries,
+  HorizontalGridLines,
+  LineSeries,
+  XAxis,
+} from 'react-vis';
 
-// import { Container } from './styles';
+class Chart extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-export default function chart() {
-  return <div />;
+  render() {
+    const data = [{ x: 0.0, y: 1519780251293 }, { x: 0.1, y: 1 }];
+    return (
+      <div className="Chart">
+        <XYPlot height={400} width={800} XAxis={[0, 1]}>
+          <HorizontalGridLines />
+          <XAxis />
+          <LineSeries data={data} />
+        </XYPlot>
+      </div>
+    );
+  }
 }
+
+export default Chart;
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
-import SplitPane from 'react-split-pane';
 import AceEditor from 'react-ace';
-import { Container } from './style';
+// Import a Mode (language)
+import 'brace/mode/java';
+// Import a Theme (okadia, github, xcode etc)
+import 'brace/theme/github';
+import SplitPane from 'react-split-pane';
+import Chart from '../../components/chart';
+
+// import { StyleContainerButton, StyleButton } from './style';
+import './style.css';
 
 export default class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      type: 'start',
+      timestamp: 1519780251293,
+      select: ['min_response_time', 'max_response_time'],
+      group: ['os', 'browser'],
+    };
+  }
+
   render() {
     return (
-      <Container>
-        <SplitPane split="horizontal">
-          <AceEditor />
-          <div style={{ backgroundColor: 'blue' }}>RIGHT</div>
-        </SplitPane>
-      </Container>
+      <SplitPane split="horizontal" minSize={210} width={700} defaultSize={200}>
+        <div>
+          <AceEditor
+            mode="java"
+            theme="github"
+            name="UNIQUE_ID_OF_DIV"
+            editorProps={{ $blockScrolling: true }}
+            value={JSON.stringify(this.state)}
+            width="900px"
+            height="200px"
+          />
+        </div>
+        <div>
+          <Chart />
+          {/* <StyleContainerButton>
+            <StyleButton> vai </StyleButton>
+          </StyleContainerButton> */}
+        </div>
+      </SplitPane>
     );
   }
 }
